7779dcd10feae119c9c52d474ff93b5b977fd3bb
Owen Plesko <owenplesko@gmail.com>
2025-05-21 20:45:06 -0400

    forgot to drop stats update timestamp col

--- server/sql/migrations/00002_drop_stats.sql
+++ server/sql/migrations/00002_drop_stats.sql
@@ -5,6 +5,7 @@
 DROP TABLE IF EXISTS tft_summoner_stats;
 DROP PROCEDURE IF EXISTS update_tft_summoner_stats (VARCHAR);
 ALTER TABLE tft_comp DROP COLUMN IF EXISTS insert_timestamp;
+ALTER TABLE tft_summoner DROP COLUMN IF EXISTS stats_update_timestamp;
 
 /* Refactor idx_name_tag_insensitive to use fn_normalize_str to be consistent */
 DROP INDEX IF EXISTS idx_name_tag_insensitive;
