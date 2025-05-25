80ba1151d77bbc533e1391d49995c241d60ffc28
Owen Plesko <owenplesko@gmail.com>
2025-05-25 17:34:44 -0400

    schedule singleton mode

--- server/go.mod
+++ server/go.mod
@@ -3,13 +3,18 @@ module github.com/owenplesko/TftAnalytics
 go 1.24.0
 
 require (
+	github.com/go-co-op/gocron/v2 v2.16.2
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/joho/godotenv v1.5.1
 	github.com/pressly/goose/v3 v3.24.2
 	github.com/redis/go-redis/v9 v9.7.3
 )
 
-require github.com/robfig/cron/v3 v3.0.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
--- server/internal/schedule/schedule.go
+++ server/internal/schedule/schedule.go
@@ -3,31 +3,39 @@ package schedule
 import (
 	"context"
 
+	"github.com/go-co-op/gocron/v2"
 	"github.com/owenplesko/TftAnalytics/internal/services"
-	"github.com/robfig/cron/v3"
 )
 
 type Schedule struct {
-	c       *cron.Cron
-	service *services.Service
+	scheduler gocron.Scheduler
+	service   *services.Service
 }
 
 func New(service *services.Service) *Schedule {
 	ctx := context.Background()
 
-	c := cron.New()
-	c.AddFunc("0 0 * * *", func() { service.RefreshUnitPlacement(ctx) })
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		panic(err)
+	}
+
+	_, _ = scheduler.NewJob(
+		gocron.DailyJob(1, gocron.NewAtTimes(gocron.NewAtTime(0, 0, 0))),
+		gocron.NewTask(service.RefreshUnitPlacement, ctx),
+		gocron.WithSingletonMode(gocron.LimitModeReschedule),
+	)
 
 	return &Schedule{
-		c:       c,
-		service: service,
+		scheduler: scheduler,
+		service:   service,
 	}
 }
 
 func (s *Schedule) Start() {
-	s.c.Start()
+	s.scheduler.Start()
 }
 
-func (s *Schedule) Stop() {
-	s.c.Stop()
+func (s *Schedule) Shutdown() {
+	s.scheduler.Shutdown()
 }
