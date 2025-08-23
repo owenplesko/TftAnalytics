9675ab14f7b57ec74a12efcf34ff89598dc87035
Owen Plesko <owenplesko@gmail.com>
2025-08-22 21:39:40 -0400

    header sticky

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -11,10 +11,10 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   {
     component: () => (
       <>
-        <header className="flex w-full bg-card px-4 py-2">
+        <header className="sticky top-0 z-10 flex w-full bg-background px-4 py-2">
           <nav className="mr-auto flex items-start gap-2">
             <Link to="/units">
-              <Button variant="ghost">Units</Button>
+              <Button variant="link">Units</Button>
             </Link>
           </nav>
           <SearchForm />
